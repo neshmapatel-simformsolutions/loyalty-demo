2f54268688dc822ef42c37b9b207c7db6ccd0dc5
neshmapatel-simformsolutions <neshma.p@simformsolutions.com>
2022-03-30 00:09:43 +0530

    feat: add predefined currency values

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Currency.destroy_all
+[
+  [1, 'USD', '$'],
+  [2, 'CAD', '$'],
+  [3, 'AUD', '$'],
+  [4, 'NZD', '$'],
+  [5, 'INR', '₹']
+].each do |currency|
+  Currency.create!(
+    id: currency[0],
+    name: currency[1],
+    code: currency[2]
+  )
+end
+
