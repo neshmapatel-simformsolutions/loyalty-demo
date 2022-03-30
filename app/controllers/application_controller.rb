be2db311e733b1b49b2868333a31878645925fd8
neshmapatel-simformsolutions <neshma.p@simformsolutions.com>
2022-03-30 17:42:20 +0530

    fix: bug in devise implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  layout :layout
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthdate,:first_name,:last_name,:currency_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:birthdate,:first_name,:last_name,:currency_id])
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
+  def layout
+    # only turn it off for devise pages:
+    if devise_controller?
+      false
+    else
+      "application"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'users/show'
+  root :to => "users#show"
 
   scope module: "api" do
     namespace :v1 do
