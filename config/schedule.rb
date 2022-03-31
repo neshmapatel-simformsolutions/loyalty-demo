94ade0d09b2d75bea16110629a1b8add7f8e77c7
neshmapatel-simformsolutions <neshma.p@simformsolutions.com>
2022-04-01 01:50:28 +0530

    feat: quaterly rewards for user

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@
 
 set :output, "log/cron.log"
 
-# runs every first day of month at 8am. Updates free_coffee rewards
+# runs every last day of month at 8am. Updates free_coffee rewards
 every '0 8 28-31 * *' do
   rake 'free_coffee:free_coffee_reward_at_end_of_month'
 end
@@ -20,4 +20,9 @@ end
 # runs every first of the month at 9 am
 every '0 9 1 * *' do
   rake 'birthday_reward:reward_user_on_birthday'
+end
+
+# runs every calendar quarterly at 11 pm i.e. on last day of every 3 months at 11 pm.
+every '0 23 28-31 */3 *' do
+  rake 'quaterly_rewards:bonus_points_to_user'
 end
---
+++ lib/tasks/quaterly_rewards.rake
@@ -0,0 +1,18 @@
+namespace :quaterly_rewards do
+
+  # credit 100 bonus points to users having transactions amount more than 2000 in last 3 months
+  task bonus_points_to_user: :environment do
+
+    User.includes(:transactions).where(
+      transactions: {created_at: Date.today.months_since(-3).beginning_of_day..Date.today.end_of_day}
+    ).find_each(batch_size: 1000) do |user|
+
+      # exclude foreign transactions
+      if user.fetch_native_transactions.sum(:amount) > 2000
+        user.points = user.points + 100
+        user.save!
+      end
+    end
+  end
+
+end
